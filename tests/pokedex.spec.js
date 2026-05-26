8c0ecd2c980b26208534e30b78f91b520fe603a6
Christoph2497 <cmb-an@gmx.de>
2026-05-26 17:32:37 +0200

    add more e2e tests

--- tests/pokedex.spec.js
+++ tests/pokedex.spec.js
@@ -1,8 +1,8 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080')
+    await page.goto('')
 
     await expect(page.getByText('ivysaur')).toBeVisible()
 
@@ -11,5 +11,11 @@ describe('Pokedex', () => {
         'Pokémon and Pokémon character names are trademarks of Nintendo.',
       ),
     ).toBeVisible()
+  })
+
+  test('click on pokemon', async ({ page }) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
